43b25ea65a9c3aba450dc77b4e4053ef610e071a
Daniel Duque <danielvduque@gmail.com>
2022-07-11 13:38:08 -0400

    Connection to DB fixed. put endpoint added

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
   useUnifiedTopology: true
 });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully to database: ", dbName);
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected successfully to database: ', dbName);
 
   // Google app engine 
   app.set('trust proxy', true);
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const Expense = require('../models/Expense')
+const Expense = require('../models/Expense');
 
 router.get('/expenses', (req, res) => {
   res.status(200).json({
-    message: "message"
+    message: 'message'
   });
 });
 
@@ -35,17 +35,49 @@ router.post('/expenses', postValidations, async (req, res) => {
     if (err) {
       console.log(err);
       res.status(400).json({
-        message: "Error saving a new expense",
+        message: 'Error saving a new expense',
         description: err
       });
     }
 
     res.status(200).json({
-      message: "Expense data registered successfully",
+      message: 'Expense data registered successfully',
       id: exp._id,
       datetime: exp.datetime
     });
   });
+});
+
+
+const putValidations = [
+  check('id').notEmpty().withMessage('ID is required').isLength({ min: 24, max:24 }).withMessage('id must be a 24 string length')
+];
+router.put('/expenses', putValidations, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors);
+  }
+
+  let dataToUpdate = {};
+  req.body.total ? dataToUpdate.total = req.body.total : null;
+  req.body.currency ? dataToUpdate.currency = req.body.currency : null;
+  req.body.type ? dataToUpdate.type = req.body.type : null;
+  req.body.where ? dataToUpdate.where = req.body.where : null;
+  req.body.datetime ? dataToUpdate.datetime = req.body.datetime : null;
+  req.body.description ? dataToUpdate.description = req.body.description : null;
+
+  console.log('Data to update ', dataToUpdate);
+  try {
+    const expense = await Expense.findByIdAndUpdate(req.body.id, dataToUpdate);
+    let message = expense ? 'Updated successfully' : 'Something happened. please verify';
+    res.status(200).json({
+      id: req.body.id,
+      message
+    }); 
+  }catch (e){
+    console.error(e);
+    res.status(200).json(e);
+  }
 });
 
 router.get('/expenses/month', (req, res) => {
